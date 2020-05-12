388cd8f056c708147fc2b4634946064412ce3866
logan248 <logananderson248@gmail.com>
2020-05-12 10:23:50 +0000

    Changed url

--- scrape.py
+++ scrape.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-url = 'https://www.jumia.com.gh/laptops/'
+url = 'https://www.imdb.com/?mode=desktop&ref_=m_ft_dsk'
 
 
 request = requests.get(url)
 
 page = bs(request.content, 'html.parser')
 print(page.prettify())
-file_name = 'jumia_laptops.html'
+
+file_name = 'imdb.html'
 
 with open(file_name, 'w') as f:
     f.write(page.prettify())
